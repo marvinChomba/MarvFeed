1e0785a7231afb8da2a47eb25b860145bdb812db
Marvin Chomba <marvin.chomba24@gmail.com>
2018-11-09 15:05:45 +0300

    Add the models files

---
+++ app/models.py
@@ -0,0 +1,23 @@
+class Article:
+    """
+    This is the class we will use to create our articles objects
+    """
+    def __init__(self,source,author,title,description,url,image_url,publish_date,content):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image_url = image_url
+        self.publish_date = publish_date
+        self.content = content
+
+class Source:
+    """
+    This is the class we will use to create our sources objects
+    """
+    def __init__(self,id,name,description,country):
+        self.id = id
+        self.name = name
+        self.description = description  
+        self.country = country
